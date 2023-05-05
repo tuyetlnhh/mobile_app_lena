120eb2d0e7a2ef4d3fd1c92ec2bd9be73ce44d79
Duy - UIT <88521480+thaiduy132@users.noreply.github.com>
2023-05-05 17:50:13 +0700

    add some unnecessary files

--- app/src/main/java/com/example/app_mobile_lena/MainActivity.java
+++ app/src/main/java/com/example/app_mobile_lena/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.app_mobile_lena;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_detail);
-        RatingBar ratingbar = findViewById(R.id.ratingBar);
-        ratingbar.setRating(3.67f);
+
+        Button button1 = findViewById(R.id.btnShowDescription);
+        Button button2 = findViewById(R.id.btnShowReviews);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.flFragment, new Fragment1());
+                fragmentTransaction.commit();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.flFragment, new ProductDescription_Fragment());
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/app_mobile_lena/ProductDescription_Fragment.java
@@ -0,0 +1,46 @@
+package com.example.app_mobile_lena;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductDescription_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductDescription_Fragment extends Fragment {
+    public ProductDescription_Fragment() {
+        // Required empty public constructor
+    }
+    public static ProductDescription_Fragment newInstance(String param1, String param2) {
+        ProductDescription_Fragment fragment = new ProductDescription_Fragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_description_, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
