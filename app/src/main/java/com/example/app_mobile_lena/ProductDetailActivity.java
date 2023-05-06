de86ece44502bfd6a4dbe127aad23ed521b8f26c
Duy - UIT <88521480+thaiduy132@users.noreply.github.com>
2023-05-06 10:51:49 +0700

    add ProductDetailActivity

---
+++ app/src/main/java/com/example/app_mobile_lena/ProductDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.app_mobile_lena;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductDetailActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_detail);
+
+    }
+}
---
+++ app/src/main/java/com/example/app_mobile_lena/ProductReviews_Fragment.java
@@ -0,0 +1,36 @@
+package com.example.app_mobile_lena;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductReviews_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductReviews_Fragment extends Fragment {
+
+
+    public ProductReviews_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_reviews_, container, false);
+    }
+}
