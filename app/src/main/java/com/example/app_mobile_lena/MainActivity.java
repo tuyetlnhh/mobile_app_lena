fef4f4e068b244f38cd1f97555b6968dcb1556a1
Duy - UIT <88521480+thaiduy132@users.noreply.github.com>
2023-05-07 16:29:09 +0700

    delete some line in main

--- app/src/main/java/com/example/app_mobile_lena/MainActivity.java
+++ app/src/main/java/com/example/app_mobile_lena/MainActivity.java
@@ -1,26 +1,10 @@
 package com.example.app_mobile_lena;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import androidx.fragment.app.Fragment;
-
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.content.res.ColorStateList;
-import android.media.Image;
 import android.os.Bundle;
 
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.RatingBar;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
@@ -28,26 +12,14 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import java.util.ArrayList;
 import java.util.List;
 
-
-import com.google.android.material.tabs.TabLayout;
-import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
-
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.product_detail);
-
-        Button btnShowDescription = findViewById(R.id.btnShowDescription);
-        Button btnShowReview = findViewById(R.id.btnShowReviews);
-
-        RatingBar ratingBar = findViewById(R.id.ratingBar);
-        ratingBar.setRating(5f);
-
         setContentView(R.layout.activity_main);
+
         AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
 // Create items
@@ -70,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(0);
         bottomNavigation.setOnTabSelectedListener((position, wasSelected) -> {
-
             viewPager.setCurrentItem(position);
             return true;
         });
