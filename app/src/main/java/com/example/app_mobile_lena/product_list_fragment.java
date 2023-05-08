447a5dee1a15318dd7652673a6d8d88998530c12
Duy - UIT <88521480+thaiduy132@users.noreply.github.com>
2023-05-09 01:25:31 +0700

    modified

--- app/src/main/java/com/example/app_mobile_lena/product_list_fragment.java
+++ app/src/main/java/com/example/app_mobile_lena/product_list_fragment.java
@@ -79,7 +79,6 @@ public class product_list_fragment extends Fragment {
         gview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
                 Intent intent = new Intent(getActivity(), ProductDetailActivity.class);
                 startActivity(intent);
             }
