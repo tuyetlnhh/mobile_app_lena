d80f921f2baf4bd1488ab63354b9dc318d011890
Hoang - UIT <nhoang200260@gmail.com>
2023-05-09 16:34:26 +0700

    fix UI

--- app/src/main/java/com/example/app_mobile_lena/pre_login.java
+++ app/src/main/java/com/example/app_mobile_lena/pre_login.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class pre_login extends AppCompatActivity {
 
     Button btnLogin;
     Button btnRegister;
+    ImageButton btBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,8 @@ public class pre_login extends AppCompatActivity {
         setContentView(R.layout.pre_login);
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnRegister =(Button) findViewById(R.id.btnRegister);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btBack = (ImageButton) findViewById(R.id.btBack);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +36,13 @@ public class pre_login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(pre_login.this, RegisterPage.class);
                 startActivity(intent);
+            }
+        });
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
