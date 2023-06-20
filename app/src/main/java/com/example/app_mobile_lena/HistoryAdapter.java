755f4255928f6fa10f59d9f76b80761891cc7d62
huylamuit <20521389@gm.uit.edu.vn>
2023-06-20 11:42:19 +0700

    create shoping history

---
+++ app/src/main/java/com/example/app_mobile_lena/HistoryAdapter.java
@@ -0,0 +1,84 @@
+package com.example.app_mobile_lena;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class HistoryAdapter  extends BaseAdapter {
+    Context context;
+    ArrayList<String> name = new ArrayList<>();
+    ArrayList<String> img = new ArrayList<>();
+    ArrayList<String> cate = new ArrayList<>();
+    ArrayList<Double> price = new ArrayList<>();
+    LayoutInflater inflater;
+    public HistoryAdapter(Context context, ArrayList<String> name, ArrayList<String> img, ArrayList<String> cate, ArrayList<Double> price){
+        Log.d("TAG","size = "+Integer.toString(name.size()));
+
+        this.context = context;
+        this.name.addAll(name);
+        this.img.addAll(img) ;
+        this.cate.addAll(cate);
+        this.price.addAll(price);
+        Log.d("TAG","this name size =" + Integer.toString(this.name.size()));
+
+    }
+
+
+    @Override
+    public int getCount(){
+        return name.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public String addThousandSeparator(Double number) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(number);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.cart_item, parent, false);
+        }
+
+        ImageView imgView = convertView.findViewById(R.id.itemImage);
+        TextView txtName = convertView.findViewById(R.id.itemName);
+        TextView txtPrice = convertView.findViewById(R.id.itemPrice);
+        TextView txtCate = convertView.findViewById(R.id.itemCate);
+
+        txtName.setText(name.get(position));
+        txtPrice.setText(addThousandSeparator(Double.valueOf(price.get(position)))+"VND");
+        txtCate.setText(cate.get(position));
+
+
+        imgView.setImageResource(R.drawable.image_1);
+//        RequestOptions requestOptions = new RequestOptions()
+//                .placeholder(R.drawable.loading_icon) // (tùy chọn) hình ảnh hiển thị trước khi tải
+//                .error(R.drawable.no_image);
+//        Glide.with(convertView)
+//                .load(img.get(position))
+//                .apply(requestOptions)
+//                .into(imgView);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/app_mobile_lena/QRScanActivity.java
@@ -0,0 +1,115 @@
+package com.example.app_mobile_lena;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.Gravity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class QRScanActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+
+        surfaceView = findViewById(R.id.surfaceView);
+
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+
+
+
+
+        // Kiểm tra quyền truy cập camera
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        } else {
+            startCamera();
+        }
+    }
+
+    private void startCamera() {
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(QRScanActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {}
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() > 0) {
+                    String qrCodeValue = barcodes.valueAt(0).displayValue;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(QRScanActivity.this, "QR Code: " + qrCodeValue, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
